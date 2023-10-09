297d5f2f2b5514edae82b1e0811a4ae2ea1532bb
Ivan Knysh <mr.rauber.ik@gmail.com>
2023-10-09 21:57:03 +0300

    Init project

---
+++ script.js
@@ -0,0 +1,22 @@
+/**
+ * 1
+ */
+console.log("********************* Завдання 1 *********************");
+let string_1 = prompt("Введіть рядок 1");
+let string_2 = prompt("Введіть рядок 2");
+let string_3 = prompt("Введіть рядок 3");
+
+console.log(`${string_1} ${string_2} ${string_3}`);
+
+/**
+ * 2
+ */
+console.log("********************* Завдання 2 *********************");
+let number = "12345";
+let result = "";
+
+for (let i = 0; i < number.length; i++) {
+	result += number[i] + " ";
+}
+
+console.log(result);
